46eef7618991d32ae778b6b049f51b91d151586e
Your Name <you@example.com>
2022-01-05 13:25:08 +0530

    the final algo

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import math
 
-
-def toBinary(a):
+def toBinary(a,key):
   l,m=[],[]
   for i in a:
-    l.append(ord(i))
+    l.append(ord(i)-key)
   for i in l:
-    m.append(int(bin(i)[2:]))
+    m.append(int((bin(i)[2:])))
   return m
 
+
+def toBinarykey(a,key):
+  l,m=[],[]
+  for i in a:
+    l.append(ord(i)+key)
+  for i in l:
+    m.append(int((bin(i)[2:])))
+  return m
 
 def toString(a):
   l=[]
@@ -16,24 +23,54 @@ def toString(a):
   for i in a:
     b=0
     c=0
-    k=int(math.log10(i))+1
+    k=int(math.log10(i))+1                                            
 
     for j in range(k):
       b=((i%10)*(2**j))   
       i=i//10
       c=c+b
     l.append(c)
+  
   for x in l:
     m=m+chr(x)
   return m
 
-word = input("enter the word to be encrypted")
-key = input("enter the key")
-cmd = input("e - ecryption d - for decryption")
+
+def toStringkey(a,key):
+  l=[]
+  m=""
+  for i in a:
+    b=0
+    c=0
+    k=int(math.log10(i))+1                                            
+
+    for j in range(k):
+      b=((i%10)*(2**j))   
+      i=i//10
+      c=c+b
+      c=c/key
+    l.append(c)
+  
+  for x in l:
+    m=m+chr(x)
+  return m
+
+word = input("enter the word to be encrypted or decrepted: ")
+key = int(input("enter the key number : "))
+cmd = input("e - ecryption d - for decryption : ")
 
 
-print("''Hello world'' in binary is ") 
-print(toBinary("Hello world 123 !@#"))
 
-print(" \n ''[1001000, 1100101, 1101100, 1101100, 1101111,]'' in string is ")
-print(toString(toBinary("Hello world 123 !@#")))
+if (cmd == 'e'):
+  
+  print(toString(toBinarykey(word,key)))
+elif (cmd == 'd'):
+  print(toString(toBinary(word,key)))
+else:
+  print("enter a valid input")
+
+#print("''Hello world'' in binary is ") 
+#print(toBinary("Hello world 123 !@#"))
+#
+#print(" \n ''[1001000, 1100101, 1101100, 1101100, 1101111,]'' in string is ")
+#print(toString(toBinary("Hello world 123 !@#")))
